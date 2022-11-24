99bd04bfcd82e36a81a761204b0130711ad4b425
tantaana <shariartanvirofficial@gmail.com>
2022-11-24 14:56:47 +0600

    haha

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -2,8 +2,45 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
+        <div className='mt-10 mx-4'>
+            <div className="card bg-red-300 shadow-xl mb-4">
+                <div className="card-body">
+                    <h2 className="text-2xl font-bold text-blue-500">Q1. What are the different ways to manage a state in a React application?</h2>
+                    <p className='text-lg font-bold'>Ans: There are four ways to properly manage a state in your React apps:<br />
+                        <br />
+                        1. Local state<br />
+                        2. Global state<br />
+                        3. Server state<br />
+                        4. URL state</p>
+                </div>
+            </div>
 
+            <div className="card bg-blue-400 shadow-xl mb-4">
+                <div className="card-body">
+                    <h2 className="text-2xl font-bold text-yellow-400">Q2. How does prototypical inheritance work?</h2>
+                    <p className='text-lg font-bold'>
+                        Ans: The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects.<br /> It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object. getPrototypeOf and Object.</p>
+                </div>
+            </div>
+
+            <div className="card bg-green-400 shadow-xl mb-4">
+                <div className="card-body">
+                    <h2 className="text-2xl font-bold text-red-600">Q3. What is a unit test? Why should we write unit tests?</h2>
+                    <p className='text-lg font-bold'>
+                        Ans: Unit Testing is a testing method that tests an individual unit of software in isolation. Unit testing for React Apps means testing an individual React Component. Unit Testing is important for React Apps, as it helps in testing the individual functionality of React components.<br />
+                        We should write unit tests because well-written unit tests act as documentation for your code. Any developer can quickly look at your tests and know the purpose of your functions. It simplifies the debugging process. Unit testing is an integral part of extreme programming.</p>
+                </div>
+            </div>
+
+            <div className="card bg-gray-400 shadow-xl mb-4">
+                <div className="card-body">
+                    <h2 className="text-2xl font-bold text-white">Q4. React vs. Angular vs. Vue?</h2>
+                    <p className='text-lg font-bold'>
+                        Ans: All of these frameworks are totally fine to be used in a front-end application.<br /> It is true that for big projects, maybe Angular is the best candidate because it gives you a solid structure to be followed and this can help us to have an organized and maintainable base code. Nevertheless, with the proper knowledge of architecture and web development concepts, we can have an organized code with any of these frameworks.<br />
+
+                        Also, the three frameworks have an active community with a lot of documentation, support, and lots of courses available. Regarding performance, they are all pretty similar as well.</p>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -11,15 +11,16 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-yellow-600 rounded-box w-52">
-                            <li><Link className='text-white font-bold text-lg'>Home</Link></li>
+                            <li><Link to='/' className='text-white font-bold text-lg'>Home</Link></li>
                             <li><Link className='text-white font-bold text-lg'>Item 3</Link></li>
+                            <li><Link to='/blogs' className='text-white text-lg font-bold'>Blogs</Link></li>
                         </ul>
                     </div>
-                    <Link className="btn btn-ghost normal-case text-3xl font-bold italic text-white">🛵 Gari <span className='text-emerald-300'>Kino</span><span className='text-white text-2xl'>.com</span></Link>
+                    <Link className="btn btn-ghost border-0 normal-case text-3xl font-bold italic text-white">🛵 Gari <span className='text-emerald-300'>Kino</span><span className='text-white text-2xl'>.com</span></Link>
                 </div>
                 <div className="navbar-center hidden lg:flex text-white">
                     <ul className="menu menu-horizontal p-0">
-                        <li className='text-white text-xl font-bold'><Link>Home</Link></li>
+                        <li><Link to='/' className='text-white text-xl font-bold'>Home</Link></li>
                         <li><Link className='text-white text-xl font-bold'>Item 3</Link></li>
                         <li><Link to='/blogs' className='text-white text-xl font-bold'>Blogs</Link></li>
                     </ul>
