4ee684f52640068a7229648d4b27f696c30d8ace
tantaana <shariartanvirofficial@gmail.com>
2022-11-27 01:06:35 +0600

    anoterr

--- src/Pages/Dashboard/AddProduct/AddProduct.js
+++ src/Pages/Dashboard/AddProduct/AddProduct.js
@@ -1,20 +1,105 @@
-import React, { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { AuthContext } from '../../../contexts/AuthProvider';
 
 const AddProduct = () => {
+
+    const { user } = useContext(AuthContext);
 
     const { register, formState: { errors }, handleSubmit } = useForm();
     const [data, setData] = useState('');
 
+    const imageHostKey = process.env.REACT_APP_imgbb_key;
+
+
+    const { data: categories = [] } = useQuery({
+        queryKey: ['category'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/categoryName');
+            const data = await res.json();
+            return data;
+        }
+    })
+
 
     const handleAddProduct = data => {
-        console.log(data)
+        console.log(data.category)
+        const image = data.photo[0];
+        const formData = new FormData();
+        formData.append('image', image);
+        const url = `https://api.imgbb.com/1/upload?key=${imageHostKey}`
+
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(imgData => {
+                if (imgData.success) {
+
+                    const addProduct = {
+                        category: data.category,
+                        name: data.name,
+                        email: data.email,
+                        photo: imgData.data.url,
+                        price: data.price,
+                        conditionType: data.conditionType,
+                        mobile: data.mobile,
+                        productLocation: data.productLocation,
+                        description: data.description,
+                        purchaseYear: data.purchaseYear
+
+
+                    }
+
+
+                    //save product information to the database
+                    fetch('http://localhost:5000/products', {
+                        method: 'POST',
+                        headers: {
+                            'content-type': 'application/json',
+                            authorization: `bearer ${localStorage.getItem('accessToken')}`
+                        },
+                        body: JSON.stringify()
+                    })
+                        .then(res => res.json())
+                        .then(data => {
+                            if (data.acknowledged) {
+                                toast.success('Product Added Successfully')
+                            }
+                        })
+                }
+            })
     }
 
     return (
         <div className='mt-10 m-4'>
             <h2 className='text-3xl font-bold mb-6'>Add A Product</h2>
             <form onSubmit={handleSubmit(handleAddProduct)}>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text font-bold">Logged In User 🛻</span>
+                    </label>
+                    <input type="email" {...register("email")} defaultValue={user?.email} disabled className="input input-bordered input-secondary w-full max-w-xs" />
+                </div>
+
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text font-bold">Product Category:</span>
+                    </label>
+
+                    <select {...register("category")} className="select select-primary w-full max-w-xs">
+                        <option defaultValue>Honda</option>
+                        <option>Toyota</option>
+                        <option>BMW</option>
+                        {/* {
+                            categories.map((category, i) => <option key={category._id} defaultValue>{category.categories}</option>)
+                        } */}
+                    </select>
+                </div>
+
                 <div className="form-control w-full max-w-xs">
                     <label className="label">
                         <span className="label-text font-bold">Product Name 🛻</span>
@@ -25,9 +110,9 @@ const AddProduct = () => {
 
                 <div className="form-control w-full max-w-xs">
                     <label className="label">
-                        <span className="label-text font-bold">Photo URL 📸</span>
+                        <span className="label-text font-bold">Product Image 📸</span>
                     </label>
-                    <input type="text" {...register("photo", { required: 'Photo URL is required' })} placeholder="Type Your Photo Link" className="input input-bordered input-primary w-full max-w-xs" />
+                    <input type="file" {...register("photo", { required: 'Product image is required' })} placeholder="Upload your image" className="file-input file-input-bordered file-input-primary w-full max-w-xs" />
                     {errors.photo && <p className='text-red-500 font-bold mt-4 text-center'>{errors.photo?.message}</p>}
                 </div>
 
--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -1,18 +1,49 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Products = () => {
     const { categories } = useLoaderData();
-    // const [categories, setCategories] = useState([]);
-    // useEffect(() => {
-    //     fetch('http://localhost:5000/categories')
-    //         .then(res => res.json())
-    //         .then(data => setCategories(data))
-    // }, [])
+
+    const url = `http://localhost:5000/products?category=${categories}`
+
+    const { data: category = [] } = useQuery({
+        queryKey: ['category'],
+        queryFn: async () => {
+            const res = await fetch(url);
+            const data = await res.json();
+            return data
+        }
+    })
 
     return (
         <div>
             <h2 className='text-3xl font-bold mt-10'>Showing categories: <span className='text-red-500'>{categories}</span> </h2>
+
+            <div>
+                {
+                    category.map(product =>
+
+                        <div className="card w-96 bg-base-100 shadow-xl">
+                            <figure><img src={product.photo} alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">
+                                    {product.name}
+                                    <div className="badge badge-success">Latest</div>
+                                </h2>
+                                <p>{product.description}</p>
+                                <div className="card-actions justify-end">
+                                    <div className="badge badge-outline">Fashion</div>
+                                    <div className="badge badge-outline">Products</div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
+
+
+
         </div>
     );
 };
